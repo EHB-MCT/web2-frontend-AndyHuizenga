34f743c3819df80516c003acdf867a84308a7cfe
AndyHuizenga <andy.huizenga@student.ehb.be>
2021-12-20 20:28:50 +0100

    PageAlldrinks
    Create for collums

--- src/index.js
+++ src/index.js
@@ -1,23 +1,71 @@
-async function loadUser(user) {
-    const response = await fetchUser("https://web2-courseproject-andyh.herokuapp.com/users");
-    const users = await response.json();
+//GET USERS
 
+async function loadUsers() {
+    const response = await fetch("https://web2-courseproject-andyh.herokuapp.com/allUsers");
+    const users = await response.json();
     return users;
 
 
 }
-
-
 document.addEventListener('DOMContentLoaded', async () => {
     let users = [];
 
     try {
-        const user = await loadUser();
+        users = await loadUsers();
+
 
     } catch (e) {
-        console.log("Error")
+        console.log("Error");
         console.log(e);
     }
 
     console.log(users);
+})
+
+//GET FAVORITE DRINKS
+
+async function loadDrinks() {
+    const response = await fetch("https://web2-courseproject-andyh.herokuapp.com/favoriteDrinks");
+    const allDrinks = await response.json();
+    return allDrinks;
+
+
+}
+document.addEventListener('DOMContentLoaded', async () => {
+    let favoriteDrinks = [];
+
+    try {
+        favoriteDrinks = await loadDrinks();
+
+
+    } catch (e) {
+        console.log("Error");
+        console.log(e);
+    }
+
+    console.log(favoriteDrinks);
+})
+
+//GET API 
+
+async function getcocktails() {
+    const response = await fetch("");
+    const users = await response.json();
+    return users;
+
+
+}
+document.addEventListener('DOMContentLoaded', async () => {
+    let drinks = [];
+
+    try {
+        drinks = await getcocktails();
+
+
+    } catch (e) {
+        console.log("Error");
+        console.log(e);
+    }
+
+    console.log(drinks);
 })
