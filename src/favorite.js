2d0610b1ca0f49c5d22554cbfc7302ca6af4d82f
AndyHuizenga <andy.huizenga@student.ehb.be>
2021-12-22 17:40:26 +0100

    FavoriteDelete

--- src/favorite.js
+++ src/favorite.js
@@ -1,54 +1,93 @@
-let favoriteDrinks = [];
+let listAllDrinks = [];
+async function getData() {
+    //get the list
+
+    const response = await fetch(`https://web2-courseproject-andyh.herokuapp.com/favoriteDrinks`)
+    const data = await response.json();
+    console.log("data", data)
 
 
-window.addEventListener('load', () => {
-    console.log('page is fully loaded');
-    getfavoriteDrinks()
-});
+    console.log("hello", data);
 
-function getfavoriteDrinks() {
-    const url = 'https://web2-courseproject-andyh.herokuapp.com/favoriteDrinks';
-    // post body data 
+    data.forEach(drink => {
 
-
-    // request options
-
-
-    // send Get request
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log("step1:incoming data",data)
-            favoriteDrinks.push(data)
-            buildList()
-        }).catch(err => {
-            console.error('Error: ', err);
-        });
-
-    console.log("favoriteAdded")
-
-
-
+        listAllDrinks.push(drink)
+        console.log("drink", drink);
+        console.log("alldrinks", listAllDrinks);
+        console.log(`
+        name: ${drink.name}, 
+        ingredient1: ${drink.ingredient1}, 
+        ingredient2: ${drink.ingredient2}, 
+        ingredient3: ${drink.ingredient3}, 
+        img: ${drink.img}, 
+        amount1: ${drink.amount1},
+        amount2: ${drink.amount2},
+        amount3: ${drink.amount3},
+        instruction: ${drink.in},
+        id: ${drink._id}
+        `)
+    })
+    buildList()
 }
+getData()
 
 function buildList() {
     let html = ``;
-    favoriteDrinks.forEach(drink => {
-        console.log("step1", drink)
-        html += ` ${drink.strDrink} `
+    listAllDrinks.forEach(drink => {
+        html += ` 
+<div class="flex-container">
+    <div class="flex-item" style="justify-content:space-between;">
+        <img src="./images/icons/element-line.png" width="150px" height="10px" margin="0" alt="">
+        <img id="favouriteThumb" src="${drink.img}"
+            style="max-width: 250px; display:block; margin-left:40px; " alt="">
+    </div>
+    <div class="flex-item">
+        <div id="ingredients4">
+   
+            <div id="all1in4">
+                <img id="ingredientsImg" src="https://www.thecocktaildb.com/images/ingredients/${drink.ingredient1}-Small.png" alt="">
+                <div>
+                    <span>${drink.ingredient1}</span><br>
+                    <span>${drink.amount1}</span>
+                </div>
+            </div>
+            <div id="all1in4">
+                <img id = "ingredientsImg"
+                src = "https://www.thecocktaildb.com/images/ingredients/${drink.ingredient2}-Small.png"
+                alt = "" >
+                <div>
+                  <span>${drink.ingredient2}</span><br>
+                    <span>${drink.amount2}</span>
+                </div>
+            </div>
+            <div id="all1in4">
+                <img id = "ingredientsImg"
+                src = "https://www.thecocktaildb.com/images/ingredients/${drink.ingredient3}-Small.png"
+                alt = "" >
+                <div>
+                  <span>${drink.ingredient3}</span><br>
+                    <span>${drink.amount3}</span>
+                </div>
+            </div>
+              <div id="all1in4">
+                <img id = "ingredientsImg"
+                src = "https://www.thecocktaildb.com/images/ingredients/${drink.ingredient4}-Small.png"
+                alt = "" >
+                <div>
+                  <span>${drink.ingredient4}</span><br>
+                    <span>${drink.amount4}</span>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    <div class="flex-item" id="centerOptions">
+        <div id="optionsEl">STEPS</div>
+        <div id="optionsEl">TRIED</div>
+        <div id="optionsEl">DELETE</div>
+    </div>
+    </div>
+          `
     })
     document.getElementById('showFavorites').innerHTML = html;
-
-    document.querySelectorAll('.btn').forEach(item => {
-        item.addEventListener('click', event => {
-            //Get the id of the clicked item
-            let id = event.target.id;
-            console.log("ID of the drink", id)
-            let addToFav = listAllDrinks.find(drink => drink.idDrink == id);
-            //Add the favoriteDrink to the Array of drinks and start Fetch post
-            favoriteDrinks.push(addToFav);
-            uploadFavoriteDrinks(addToFav);
-            console.log("Array with favoriteDrinks", favoriteDrinks)
-        });
-    });
 }
